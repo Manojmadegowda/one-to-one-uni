682638f5adfd29d1f8d772df5410ac6eb0f69a4e
Manojmadegowda <manojmadegowda@gmail.com>
2022-03-28 13:05:05 +0530

    apple file is added

---
+++ src/main/java/com/ty/onetoone/dto/Apple.java
@@ -0,0 +1,13 @@
+package com.ty.onetoone.dto;
+
+public class Apple {
+	
+	public static void apple() {
+		System.out.println("method is called");
+	}
+	public static void main(String[] args) {
+		System.out.println("Testing ");
+		apple();
+	}
+
+}
